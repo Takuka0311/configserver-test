fa24dde983553f6520fedd4fd0db8f2030a3430e
Takuka0311 <1914426213@qq.com>
2023-05-26 03:32:14 +0000

    fix bug

--- script/clean.sh
+++ script/clean.sh
@@ -6,6 +6,7 @@ find ../configserver/ -name "*.log" -type f -delete
 
 # 清理ilogtail文件夹
 rm -rf ../ilogtail/checkpoint
+rm -rf ../ilogtail/remote_yaml_config.d
 rm -rf ../ilogtail/snapshot
 rm -rf ../ilogtail/user_config.d
 rm -rf ../ilogtail/user_yaml_config.d
@@ -13,5 +14,7 @@ rm -rf ../ilogtail/users
 rm -rf ../ilogtail/app_info.json
 rm -rf ../ilogtail/apsara_log_conf.json
 rm -rf ../ilogtail/backtrace.dat
+rm -rf ../ilogtail/inotify_watcher_dirs
+rm -rf ../ilogtail/plugin_logger.xml
 find ../ilogtail/ -name "*.log" -type f -delete
 find ../ilogtail/ -name "*.LOG" -type f -delete
